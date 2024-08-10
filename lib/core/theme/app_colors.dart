f7d1bf5e229a0b90f63041d27a6ec64c5f383c0a
fawaidac <fwdachmd@gmail.com>
2024-08-10 22:57:40 +0700

    first commit

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color buttonColor = Color(0XFF242626);
+  static const Color whiteColor = Colors.white;
+}
---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../theme/app_colors.dart';
+import '../theme/app_fonts.dart';
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+
+  const CustomButton({
+    Key? key,
+    required this.onPressed,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      margin: const EdgeInsets.only(top: 8),
+      width: MediaQuery.of(context).size.width,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.buttonColor,
+          shadowColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        child: Text(
+          text,
+          style: AppFonts.poppins(fontSize: 14, color: AppColors.whiteColor),
+        ),
+      ),
+    );
+  }
+}
